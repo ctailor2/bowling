e1a0bade4c8889e6ca7c39b988911d123fbf5842
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-23 17:48:25 -0600

    Frame #initialize method

--- frame.rb
+++ frame.rb
@@ -1,2 +1,8 @@
 class Frame
+	attr_reader :rolls, :pins
+
+	def initialize
+		@rolls
+		@pins
+	end
 end
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,4 +1,15 @@
 require_relative '../frame'
 
 describe Frame do
+	let(:frame) { Frame.new }
+
+	describe "#initialize" do
+		it "responds to rolls" do
+			expect(frame).to respond_to(:rolls)
+		end
+
+		it "responds to pins" do
+			expect(frame).to respond_to(:pins)
+		end
+	end
 end
