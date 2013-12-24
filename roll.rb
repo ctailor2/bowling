22a311e453a3bd962ea2c64c3fe9eb92c79c6819
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-23 18:09:56 -0600

    Roll #initialize method

--- roll.rb
+++ roll.rb
@@ -1,4 +1,8 @@
 class Roll
+	attr_reader :pins, :result
+
 	def initialize(pins)
+		@pins = pins
+		@result
 	end
 end
--- spec/roll_spec.rb
+++ spec/roll_spec.rb
@@ -1,2 +1,13 @@
 describe Roll do
+	let(:roll) { Roll.new(10) }
+
+	describe "#initialize" do
+		it "responds to #pins" do
+			expect(roll).to respond_to(:pins)
+		end
+
+		it "responds to #result" do
+			expect(roll).to respond_to(:result)
+		end
+	end
 end
