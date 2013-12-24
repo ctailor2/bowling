7fae5e3cb4862759e763b847d517b2b8479339f7
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-24 12:42:12 -0600

    Game #score method for first 9 frames

--- game.rb
+++ game.rb
@@ -20,4 +20,22 @@ class Game
 	def roll
 		current_frame.roll
 	end
+
+	def score_frame(index)
+		frame = frames[index]
+		unless frame.active
+			subsequent_rolls = frames[index + 1..-1].flat_map { |frame| frame.rolls }
+			if frame.has_strike?
+				if subsequent_rolls.length >= 2
+					frame.score = (frame.rolls + subsequent_rolls.first(2)).map { |roll| roll.result }.inject(:+)
+				end
+			elsif frame.has_spare?
+				if subsequent_rolls.length >= 1
+					frame.score = (frame.rolls + subsequent_rolls.first(1)).map { |roll| roll.result }.inject(:+)
+				end
+			else
+				frame.score = frame.rolls.map { |roll| roll.result }.inject(:+)
+			end
+		end
+	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -55,4 +55,109 @@ describe Game do
 			game.roll
 		end
 	end
+
+	describe "#score_frame" do
+		context "on active frames" do
+			it "does not set the frame's score" do
+				Kernel.stub(:rand).and_return(5)
+				frame = game.frames.first
+				frame.roll
+				game.score_frame(0)
+				expect(frame.score).to be_nil
+			end
+		end
+
+		context "on inactive frames" do
+			describe "on the first 9 frames" do
+				context "when the frame is a strike" do
+					describe "where the next frames have at least 2 rolls" do
+						context "across 1 frame" do
+							it "correctly sets the frame's score" do
+								Kernel.stub(:rand).and_return(10, 4, 3)
+								frame = game.frames.first
+								game.roll
+								game.roll
+								game.roll
+								game.score_frame(0)
+								expect(frame.score).to eq(17)
+							end
+						end
+
+						context "across 2 frames" do
+							it "correctly sets the frame's score" do
+								Kernel.stub(:rand).and_return(10, 10, 3)
+								frame = game.frames.first
+								game.roll
+								game.roll
+								game.roll
+								game.score_frame(0)
+								expect(frame.score).to eq(23)
+							end
+						end
+					end
+
+					describe "where the next frames have less than 2 rolls" do
+						context "across 1 frame" do
+							it "does not set the frame's score" do
+								Kernel.stub(:rand).and_return(10, 3)
+								frame = game.frames.first
+								game.roll
+								game.roll
+								game.score_frame(0)
+								expect(frame.score).to be_nil
+							end
+						end
+
+						context "across 2 frames" do
+							it "does not set the frame's score" do
+								Kernel.stub(:rand).and_return(10, 10)
+								frame = game.frames.first
+								game.roll
+								game.roll
+								game.score_frame(0)
+								expect(frame.score).to be_nil
+							end
+						end
+					end
+				end
+
+				context "when the frame is a spare" do
+					describe "where the next frames have at least 1 roll" do
+						it "correctly sets the frame's score" do
+							Kernel.stub(:rand).and_return(8, 2, 3)
+							frame = game.frames.first
+							game.roll
+							game.roll
+							game.roll
+							game.score_frame(0)
+							expect(frame.score).to eq(13)
+						end
+					end
+
+					describe "where the next frames have less than 1 roll" do
+						it "does not set the frame's score" do
+							Kernel.stub(:rand).and_return(8, 2)
+							frame = game.frames.first
+							game.roll
+							game.roll
+							game.score_frame(0)
+							expect(frame.score).to be_nil
+						end
+					end
+				end
+
+				context "when the frame is neither a strike nor a spare" do
+					it "correctly sets the frame's score" do
+						Kernel.stub(:rand).and_return(3, 2)
+						frame = game.frames.first
+						game.roll
+						game.roll
+						game.score_frame(0)
+						expect(frame.score).to eq(5)
+					end
+				end
+			end
+
+		end
+	end
 end
